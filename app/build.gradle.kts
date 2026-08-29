a093f25147352b7881d4f5f699835da8d1a50004
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2026-08-29 19:09:52 -0300

    chore: bump app version to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.assistentefinanceiro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.6.0"
+        versionCode = 5
+        versionName = "0.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
