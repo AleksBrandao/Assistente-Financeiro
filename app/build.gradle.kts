a0c1f9964ec642002026da0133a00e175b2b37bf
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2026-08-29 14:17:26 -0300

    chore: preparar versão 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.assistentefinanceiro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.5.0"
+        versionCode = 4
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
