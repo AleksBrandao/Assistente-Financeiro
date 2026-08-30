21c54844ecb1c9ce5f073b5d75a8cc9d0f3ffd00
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2026-08-29 22:27:45 -0300

    fix: separar valores realizados e pendentes do Mobills

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.assistentefinanceiro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.9.0"
+        versionCode = 8
+        versionName = "0.9.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
