af961f473a29aaba5eb5d79113722a81db3382e2
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2026-08-29 23:14:44 -0300

    feat: iniciar faturas de cartão

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.assistentefinanceiro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.10.0"
+        versionCode = 10
+        versionName = "0.11.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
