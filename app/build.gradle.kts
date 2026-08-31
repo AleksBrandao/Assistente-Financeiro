5d93ee1823affeffbe077ebfea1888f335020717
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2026-08-30 21:22:37 -0300

    feat: adicionar ajustes financeiros e datas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.assistentefinanceiro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.14.0"
+        versionCode = 16
+        versionName = "0.15.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
